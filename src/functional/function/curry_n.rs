aa829d3de494bebcd5eebd64452f7227a6348e6d
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-04-13 11:39:52 +0100

    Move curry_n into file, implement many-arg test

---
+++ src/functional/function/curry_n.rs
@@ -0,0 +1,115 @@
+use core::marker::PhantomData;
+
+use crate::{
+    functional::Pointed,
+    hlist::{
+        cons::{ConsList, ConsPushBack, Uncons},
+        tuple::Cons,
+    },
+};
+
+use super::Function;
+
+// TODO: Test, formalize, replace regular Curry when finished
+
+pub trait CurryN<I> {
+    type Curried;
+
+    fn curry_n(self) -> Self::Curried;
+}
+
+impl<T, I> CurryN<I> for T
+where
+    T: Function<I>,
+    I: Cons,
+{
+    type Curried = CurriedN<T, (), I::Cons>;
+
+    fn curry_n(self) -> Self::Curried {
+        CurriedN(self, (), PhantomData)
+    }
+}
+
+pub struct CurriedN<F, AO, AI>(F, AO, PhantomData<AI>);
+
+impl<F, AI> Default for CurriedN<F, (), AI>
+where
+    F: Default,
+{
+    fn default() -> Self {
+        CurriedN(Default::default(), (), PhantomData)
+    }
+}
+
+impl<F, AO, AI> Clone for CurriedN<F, AO, AI>
+where
+    F: Clone,
+    AO: Clone,
+{
+    fn clone(&self) -> Self {
+        CurriedN(self.0.clone(), self.1.clone(), PhantomData)
+    }
+}
+
+impl<F, AI> Pointed for CurriedN<F, (), AI>
+where
+    F: Function<AI::Uncons>,
+    AI: ConsList,
+{
+    type Pointed = F;
+
+    fn point(unit: Self::Pointed) -> Self {
+        CurriedN(unit, (), PhantomData)
+    }
+}
+
+impl<F, AO, I, P> Function<I, P> for CurriedN<F, AO, (I, ())>
+where
+    AO: ConsPushBack<I, P>,
+    F: Function<<AO::ConsPushBack as Uncons>::Uncons>,
+{
+    type Output = F::Output;
+
+    fn call(self, input: I) -> Self::Output {
+        self.0.call(self.1.cons_push_back(input).uncons())
+    }
+}
+
+impl<F, AO, Tail, Tail2, I, P> Function<I, P> for CurriedN<F, AO, (I, (Tail, Tail2))>
+where
+    AO: ConsPushBack<I, P>,
+{
+    type Output = CurriedN<F, AO::ConsPushBack, (Tail, Tail2)>;
+
+    fn call(self, input: I) -> Self::Output {
+        CurriedN(self.0, self.1.cons_push_back(input), PhantomData)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::functional::{function::curry_n::CurryN, Function};
+
+    #[test]
+    fn test_curry_n() {
+        struct ManyArgs;
+
+        impl<A, B, C, D, E, F, G> Function<(A, B, C, D, E, F, G)> for ManyArgs {
+            type Output = (A, B, C, D, E, F, G);
+
+            fn call(self, input: (A, B, C, D, E, F, G)) -> Self::Output {
+                input
+            }
+        }
+
+        let curried = ManyArgs.curry_n();
+        let curried = curried.call(1);
+        let curried = curried.call(2.0);
+        let curried = curried.call("three");
+        let curried = curried.call(4);
+        let curried = curried.call(5.0);
+        let curried = curried.call("six");
+        let curried = curried.call(7);
+        assert_eq!(curried, (1, 2.0, "three", 4, 5.0, "six", 7));
+    }
+}
--- src/functional/function/mod.rs
+++ src/functional/function/mod.rs
@@ -2,106 +2,7 @@ mod add;
 mod compose;
 mod constant;
 mod curry;
-mod curry_n {
-    use core::marker::PhantomData;
-
-    use crate::{
-        functional::Pointed,
-        hlist::{
-            cons::{ConsList, ConsPushBack, Uncons},
-            tuple::Cons,
-        },
-    };
-
-    use super::Function;
-
-    pub trait CurryN<I> {
-        type Curried;
-
-        fn curry_n(self) -> Self::Curried;
-    }
-
-    impl<T, I> CurryN<I> for T
-    where
-        T: Function<I>,
-        I: Cons,
-    {
-        type Curried = CurriedN<T, (), I::Cons>;
-
-        fn curry_n(self) -> Self::Curried {
-            CurriedN(self, (), PhantomData)
-        }
-    }
-
-    pub struct CurriedN<F, AO, AI>(F, AO, PhantomData<AI>);
-
-    impl<F, AI> Default for CurriedN<F, (), AI>
-    where
-        F: Default,
-    {
-        fn default() -> Self {
-            CurriedN(Default::default(), (), PhantomData)
-        }
-    }
-
-    impl<F, AO, AI> Clone for CurriedN<F, AO, AI>
-    where
-        F: Clone,
-        AO: Clone,
-    {
-        fn clone(&self) -> Self {
-            CurriedN(self.0.clone(), self.1.clone(), PhantomData)
-        }
-    }
-
-    impl<F, AI> Pointed for CurriedN<F, (), AI>
-    where
-        F: Function<AI::Uncons>,
-        AI: ConsList,
-    {
-        type Pointed = F;
-
-        fn point(unit: Self::Pointed) -> Self {
-            CurriedN(unit, (), PhantomData)
-        }
-    }
-
-    impl<F, AO, I, P> Function<I, P> for CurriedN<F, AO, (I, ())>
-    where
-        AO: ConsPushBack<I, P>,
-        F: Function<<AO::ConsPushBack as Uncons>::Uncons>,
-    {
-        type Output = F::Output;
-
-        fn call(self, input: I) -> Self::Output {
-            self.0.call(self.1.cons_push_back(input).uncons())
-        }
-    }
-
-    impl<F, AO, Tail, Tail2, I, P> Function<I, P> for CurriedN<F, AO, (I, (Tail, Tail2))>
-    where
-        AO: ConsPushBack<I, P>,
-    {
-        type Output = CurriedN<F, AO::ConsPushBack, (Tail, Tail2)>;
-
-        fn call(self, input: I) -> Self::Output {
-            CurriedN(self.0, self.1.cons_push_back(input), PhantomData)
-        }
-    }
-
-    #[cfg(test)]
-    mod test {
-        use crate::functional::{function::curry_n::CurryN, Add, Function};
-
-        #[test]
-        fn test_curry_n() {
-            let curried = Add.curry_n();
-            let curried = curried.call(1);
-            let curried = curried.call(2);
-            assert_eq!(curried, 3);
-        }
-    }
-}
+mod curry_n;
 mod div;
 mod flip;
 mod id;
@@ -112,6 +13,7 @@ pub use add::*;
 pub use compose::*;
 pub use constant::*;
 pub use curry::*;
+pub use curry_n::*;
 pub use div::*;
 pub use flip::*;
 pub use id::*;
